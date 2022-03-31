5398d204274ead436e1d481e37a6425ecf792abb
Ewig <valikallstars@gmail.com>
2022-03-31 23:28:46 +0300

    first commit and added navbar

---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+import logoMob from '../assets/images/logo-mob.png';
+import logoDesk from '../assets/images/logo-desk.png';
+import iBurger from '../assets/images/i-burger.png';
+
+export default { logoMob, logoDesk, iBurger }
